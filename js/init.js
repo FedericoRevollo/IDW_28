4affa18cd04f26337e2ae3e7c8fe06986f65ec38
braiang406 <braiang.bg97@gmail.com>
2025-11-14 02:45:34 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -22,57 +22,18 @@ const DEFAULT_MEDICOS = [
     fotografia: "image/1000736539==",
     valorConsulta: 4000.00
   }
-  {
-    id: 3,
-    matricula: 67890,
-    apellido: "Alex",
-    nombre: "Otega",
-    especialidad: 4,
-    descripcion: "especialista en lesiones oseas.",
-    obrasSociales: [1, 2],
-    fotografia: "image/1000736540==",
-    valorConsulta: 4000.00
-  }
-  {
-    id: 4,
-    matricula: 67890,
-    apellido: "Fenandez",
-    nombre: "Agustina",
-    especialidad: 3,
-    descripcion: "Pediatra especializado en primeros años de vida.",
-    obrasSociales: [1, 2],
-    fotografia: "image/1000736539==",
-    valorConsulta: 4000.00
-  }
-  {
-    id: 2,
-    matricula: 67890,
-    apellido: "Sofia",
-    nombre: "Castro",
-    especialidad: 5,
-    descripcion: "Pediatra especializado en primeros años de vida.",
-    obrasSociales: [1, 2],
-    fotografia: "image/1000736539==",
-    valorConsulta: 4000.00
-  }
 ];
 
 // Especialidades
 const DEFAULT_ESPECIALIDADES = [
   { id: 1, nombre: "Clínica médica" },
   { id: 2, nombre: "Pediatría" }
-  { id: 3, nombre: "Traumatologia" }
-  { id: 4, nombre: "Neurologia" }
-  { id: 2, nombre: "Oftalmologia" }
 ];
 
 // Obras Sociales (incluye campo 'descuento' para la lógica de cálculo)
 const DEFAULT_OBRAS_SOCIALES = [
   { id: 1, nombre: "OSDE", descuento: 30 },
   { id: 2, nombre: "IOMA", descuento: 48 }
-  { id: 2, nombre: "OSPE", descuento: 25 }
-  { id: 2, nombre: "OSPRERA", descuento: 45 }
-  { id: 2, nombre: "IOPSEP", descuento: 15 }
 ];
 
 // Turnos
@@ -108,5 +69,5 @@ function inicializarStorage() {
 inicializarStorage();
 
 
-// Ejecutar la inicialización al cargar este script
+// EjecutaR la inicialización al cargar este script
 inicializarStorage();
